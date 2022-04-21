aec8ada844be10b7980e587dd166eff860c81f32
Никита Ильченко <nikita.ilchenko1998@gmail.com>
2022-04-21 22:16:28 +0300

    Fixing MakeConvolution

--- Library/MakeConvolution.cpp
+++ Library/MakeConvolution.cpp
@@ -5,7 +5,9 @@ std::vector<ComplexSamples> MakeConvolution(
     const std::vector<ComplexSamples>& second_signal) {
   int num_samples_after_conv = first_signal.size() + second_signal.size() - 1;
   std::vector<ComplexSamples> signal_after_conv(num_samples_after_conv);
-  //std::reverse(second_signal.begin(), second_signal.end());
+  //std::vector<ComplexSamples> reverse_second_signal = second_signal;
+  //std::reverse(reverse_second_signal.begin(), reverse_second_signal.end());
+
   for (auto i = 0; i < first_signal.size(); ++i) {
     for (auto j = 0; j < second_signal.size(); ++j) {
       signal_after_conv[i + j].realPart =
@@ -14,7 +16,7 @@ std::vector<ComplexSamples> MakeConvolution(
           first_signal[i].imagPart * (-second_signal[j].imagPart);
       signal_after_conv[i + j].imagPart =
           signal_after_conv[i + j].imagPart +
-          first_signal[i].realPart * (-second_signal[j].imagPart) -
+          first_signal[i].realPart * (-second_signal[j].imagPart) +
           first_signal[i].imagPart * second_signal[j].realPart;
     }
   }
